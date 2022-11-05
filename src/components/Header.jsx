06ac18cdbb7e24298a6dbba25daa62185f6235a9
Nguyễn Minh Thái <nguyenminhthai3108@gmail.com>
2022-11-06 00:48:47 +0700

    Register

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { searchPorudctByName } from "../services/ProductService";
 
 import logo from "../assets/images/gucci-gang.png";
+import { toast } from "react-toastify";
 
 const mainNav = [
   {
@@ -22,31 +24,45 @@ const mainNav = [
   },
 ];
 
-const Header = () => {
+const Header = ({ setSearchedValue }) => {
+  const [searchKeyword, setSearchKeyWord] = useState("");
   const { pathname } = useLocation();
   const activeNav = mainNav.findIndex((e) => e.path === pathname);
-
   const headerRef = useRef(null);
+  // const [login, setLogin] = useState("/");
 
-  // useEffect(() => {
-  //   window.addEventListener("scroll", () => {
-  //     if (
-  //       document.body.scrollTop > 80 ||
-  //       document.documentElement.scrollTop > 80
-  //     ) {
-  //       headerRef.current.classList.add("shrink");
+  // const logOutHandeler = () => {
+  //   const user = JSON.parse(localStorage.getItem("user"));
+  //   if (user) {
+  //     var expiration = Number(user.expiration);
+  //     var today = Math.round(new Date().getTime());
+  //     if (today < expiration) {
+  //       setLogin("/login");
   //     } else {
-  //       headerRef.current.classList.remove("shrink");
+  //       setLogin("/");
   //     }
-  //   });
-  //   return () => {
-  //     window.removeEventListener("scroll");
-  //   };
-  // }, []);
+  //   } else {
+  //     setLogin("/");
+  //   }
+  // };
 
   const menuLeft = useRef(null);
 
   const menuToggle = () => menuLeft.current.classList.toggle("active");
+
+  const searchProduct = (event) => {
+    event.preventDefault();
+    searchPorudctByName(searchKeyword)
+      .then((result) => {
+        if (result) {
+          console.log(result);
+        }
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+        console.log(err);
+      });
+  };
 
   return (
     <div className="header" ref={headerRef}>
@@ -80,14 +96,26 @@ const Header = () => {
           </div>
           <div className="header__menu__right">
             <div className="header__menu__item header__menu__right__item">
-              <i className="bx bx-search"></i>
+              <form onSubmit={(event) => searchProduct(event)}>
+                <input
+                  onChange={(event) => {
+                    setSearchKeyWord(event.target.value);
+                  }}
+                  type="text"
+                  placeholder="Search.."
+                  name="search"
+                />
+              </form>
             </div>
             <div className="header__menu__item header__menu__right__item">
               <Link to="/cart">
                 <i className="bx bx-shopping-bag"></i>
               </Link>
             </div>
-            <div className="header__menu__item header__menu__right__item">
+            <div
+              // onClick={() => logOutHandeler()}
+              className="header__menu__item header__menu__right__item"
+            >
               <Link to="/login">
                 <i className="bx bx-user"></i>
               </Link>
